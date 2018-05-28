d9bcdb2144f0e2eb199a0c1a5ea5c0984468e8ea
Lavande <admin@5dimn.com>
2018-05-28 21:10:43 +0800

    minor bug fixes

--- danmu.py
+++ danmu.py
@@ -1,15 +1,6 @@
 from flask import Flask, request, render_template
 from flask_socketio import SocketIO
-import hashlib
-import xmltodict
-import time
 
-
-##BASIC SETTINGS##
-#set the server's url here
-urlbase = 'http://'
-#set your token here
-token = ''
 
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
@@ -19,63 +10,24 @@ async_mode = 'eventlet'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, async_mode=async_mode)
-thread = None
+socketio = SocketIO(app)
+
 
 
 @app.route('/danmu')
 def index():
     return render_template('danmu.html', async_mode=socketio.async_mode)
 
-def Isfromwx(request):
-	signature = request.args.get('signature', '')
-	timestamp = request.args.get('timestamp', '')
-	nonce = request.args.get('nonce', '')
-	#echostr = request.args.get('echostr', '')
-	L = [token, timestamp, nonce]
-	L.sort()
-	s = L[0] + L[1] + L[2]
-	s = s.encode('utf8')
-	if hashlib.sha1(s).hexdigest() == signature:
-		return True
-	else:
-		return False
+@app.route('/trigger')
+def trigger():
+    danmu = request.args.get('danmu', '')
+    print(danmu)
+    socketio.emit('my_response',
+        {'data': danmu},
+        namespace='/channel')
+    return 'emitted!'
+    
 
-def xml_msg(user, msg, fromuser):
-	'''format the raw message into xml'''
-
-	template = '''
-<xml>
-<ToUserName><![CDATA[{0}]]></ToUserName>
-<FromUserName><![CDATA[{3}]]></FromUserName>
-<CreateTime>{1}</CreateTime>
-<MsgType><![CDATA[text]]></MsgType>
-<Content><![CDATA[{2}]]></Content>
-</xml>'''
-	return template.format(user, int(time.time()), msg, fromuser)
-
-@app.route('/', methods=['POST', 'GET'])
-def get_msg():
-	if Isfromwx(request):
-		if request.method == 'POST':
-			d = request.data
-			d = str(d, 'utf-8')
-			d = xmltodict.parse(d)
-			FromUserName = d['xml']['FromUserName']
-			MsgType = d['xml']['MsgType']
-			me = d['xml']['ToUserName']
-
-			if MsgType == 'text':
-				with open('user_msg', 'a') as f: f.write(str(d)+'\n')
-				socketio.emit('my_response',
-						{'data': d['xml']['Content']},
-						namespace='/test')
-
-				#default auto-reply if we received a non-image message
-				msg = '弹幕已上墙，请看大屏幕！'
-				xml = xml_msg(FromUserName, msg, me)
-				return xml
-
-	#show a blank page for website visitors
-	if request.method == 'GET':
-		return ''
+if __name__ == '__main__':
+    socketio.run(app)
+    #app.run(debug=True, port=5005, host='127.0.0.1')
