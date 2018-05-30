74dd607f709130afaa513ff5fa63e2ac9c467a88
Lavande <admin@5dimn.com>
2018-05-30 16:51:54 +0800

    fix bug with group identifier

--- trigger.py
+++ trigger.py
@@ -6,7 +6,7 @@ import requests
 @itchat.msg_register(TEXT, isGroupChat=True)
 def text_reply(msg):
     #Add your own filter or remove this condition if you want to show all messages
-    if msg.fromUserName == '@@9397baca73713aca94bf99a304a9c4884d42e4be67b061d987be0aeaaeac851b':
+    if msg.User.NickName == '草地6月 清·轻 最可爱班级':
         payload = {'danmu': msg.text}
         requests.get('http://127.0.0.1:5000/trigger', params=payload)
 
